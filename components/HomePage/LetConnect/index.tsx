d4cec4a74416027ee354e052979aefadbd0da970
Huynhnt95 <huynhnt6995@gmail.com>
2021-04-26 11:18:14 +0700

    add lets connect in home page

---
+++ components/HomePage/LetConnect/index.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import styled from "styled-components";
+import { PageLayout } from "../../layout/PageLayout";
+import Form from "react-bootstrap/Form";
+import { PriamyButton } from "../../button/PriamyButton";
+
+export const LetConnect = () => {
+    return (
+        <PageLayout
+            containerStyle={{
+                background: `url("assets/images/img_contact_background.png") no-repeat left top`,
+                backgroundColor: "#FAFCFF",
+            }}
+        >
+            <div
+                className="col align-self-center align-items-center"
+                style={{ paddingTop: 70, paddingBottom: 70 }}
+            >
+                <Title>Let's connect</Title>
+                <Subtitle>
+                    Sign up to stay in touch. We'll keep you posted on new
+                    feature releases and promotions.
+                </Subtitle>
+                <Form
+                    style={{
+                        display: "flex",
+                        minWidth: 350,
+                        justifyContent: "center",
+                    }}
+                >
+                    <Form.Group
+                        controlId="formBasicEmail"
+                        style={{ width: "35%", minWidth: 320 }}
+                    >
+                        <Form.Control
+                            style={{ borderRadius: 9 }}
+                            type="email"
+                            placeholder="Enter your email here"
+                        />
+                    </Form.Group>
+                    <PriamyButton
+                        label="Sign me up"
+                        styles={{
+                            background:
+                                "linear-gradient(180deg, #6065EA 0%, #3A3EA6 100%)",
+                            marginLeft: 10,
+                            color: "#fff",
+                            height: 38,
+                            borderRadius: 9,
+                        }}
+                    ></PriamyButton>
+                </Form>
+            </div>
+        </PageLayout>
+    );
+};
+
+const Title = styled.p`
+    font-family: "Poppins";
+    font-weight: 500;
+    font-size: 32px;
+    text-align: center;
+    color: #303030;
+    margin-bottom: 10px;
+`;
+
+const Subtitle = styled.label`
+    font-family: "Poppins";
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    width: 100%;
+    text-align: center;
+    color: #606060;
+    margin-top: 0px;
+    margin-bottom: 35px;
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { CloudOnPrem } from "../components/HomePage/CloudOnPrem";
 import { ContactForm } from "../components/HomePage/FormContact";
 import { HeaderBanner } from "../components/HomePage/HeaderBanner";
 import { IndustryConcepts } from "../components/HomePage/IndustryConcepts";
+import { LetConnect } from "../components/HomePage/LetConnect";
 import { ListCompany } from "../components/HomePage/ListCompany";
 import { Principled } from "../components/HomePage/Principled";
 import { RecentArticles } from "../components/HomePage/RecentArticles";
@@ -22,6 +23,7 @@ export default function Home() {
             <Principled />
             <ContactForm />
             <RecentArticles />
+            <LetConnect />
         </div>
     );
 }
